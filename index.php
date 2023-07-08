30ee478df88935ee3c3e695f0b7ff58afb193100
Rybensx <anderson_arf@hotmail.es>
2023-07-07 22:16:08 -0600

    ramos_tarea1

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ramos_tarea1</title>
+</head>
+<body>
+    <h1>Ramos_tarea1</h1>
+
+    <script src="./Ramos_tarea1.js"></script>
+</body>
+</html>
